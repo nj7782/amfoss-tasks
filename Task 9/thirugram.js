7213ab966160c4bd5225c5a26a008851afa0fbb5
Jitin <73245324+nj7782@users.noreply.github.com>
2020-11-24 14:33:29 +0530

    Add files via upload

---
+++ Task 9/thirugram.js
@@ -0,0 +1,19 @@
+//spam message the chats with thirugram.js 
+//open up telegram web and go to the chat you want to spam 
+//open console ( Ctrl + Shift + J )
+//execute the code 
+var message = ""; //spam message 
+var interval = 1  ; // in seconds 
+var count = 10 ; //number of times to send
+var notifyInterval = 5 ; //notify 
+var i = 0 ;
+var timer = setInterval(function(){
+	document.getElementsByClassName('composer_rich_textarea')[0].innerHTML = message;
+	$('.im_submit').trigger('mousedown');	
+	i++;
+	if( i  == count )
+	clearInterval(timer);
+	if( i % notifyInterval == 0)
+	console.log(i + ' MESSAGES SENT');
+} , interval * 1000 ) ;
+//BTW , this is how I sent 10,000 Happy birthday wishes to my friend.
