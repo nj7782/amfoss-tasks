9c8584fbcb77ed5482b3744765ed0fbfcfa33523
Jitin <73245324+nj7782@users.noreply.github.com>
2021-03-22 20:09:44 +0530

    Add files via upload

---
+++ Task 6/njgeddit.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/vartanbeno/go-reddit/reddit"
+)
+
+var ctx = context.Background()
+
+func main() {
+	if err := run(); err != nil {
+		log.Fatal(err)
+	}
+}
+func run() (err error) {
+	withCredentials := reddit.WithCredentials("ID", "sec", "user", "pass")
+	client, _ := reddit.NewClient(withCredentials)
+	posts, _, err := client.Subreddit.TopPosts(ctx, "memes", &reddit.ListPostOptions{
+		ListOptions: reddit.ListOptions{
+			Limit: 100,
+		},
+		Time: "week",
+	})
+
+	if err != nil {
+		return
+	}
+	for _, post := range posts {
+		_, err := client.Post.Upvote(ctx, "t3_"+post.ID)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+	return
+}
