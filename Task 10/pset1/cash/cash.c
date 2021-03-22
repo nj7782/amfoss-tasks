dcb8eabbe62421e757699e752890e65e62cdcf2a
Jitin <73245324+nj7782@users.noreply.github.com>
2021-03-22 16:27:01 +0530

    Update cash.c

--- Task 10/pset1/cash/cash.c
+++ Task 10/pset1/cash/cash.c
@@ -1,46 +1,32 @@
-#include <cs50.h>
-#include <stdio.h>
-#include <math.h>
+#include<stdio.h>
+#include<cs50.h>
+#include<math.h>
 
-int main(void)
+int main()
 {
-    // asks for change owed and validates input, only accepts positive numbers
     float dollars;
-    do
-    {
-        dollars = get_float("Change owed in dollars: ");
+    do{
+    dollars=get_float("change owed:");
     }
-    while (dollars <= 0);
-    
-    int coins = 0;
-    // converts from float to int for precision
-    int cents = round(dollars * 100);
-    
-    // loop continues until you are owed no change
-    while (cents > 0)
+    while(dollars<0);
+    int cents = round(dollars*100);
+    int coins=0;
+
+    while(cents!=0)
     {
-        // conditionals check for every type of coin, subtract that coin from the total change and add 1 to the total number of coins
-        if (cents >= 25)
-        {
-            cents -= 25;
-            coins++;
-        }
-        else if (cents >= 10)
-        {
-            cents -= 10;
-            coins++;
-        }
-        else if (cents >= 5)
-        {
-            cents -= 5;
-            coins++;
-        }
-        else
-        {
-            cents -= 1;
-            coins++;
-        }
+        coins+=cents/25;
+        cents = cents%25;
+
+         coins+=cents/10;
+         cents %= 10;
+
+         coins+=cents/5;
+         cents %= 5;
+
+         coins+=cents/1;
+         cents %= 1;
+
     }
-    // displays the total number of coins needed to give the change
-    printf("you are owed %i coins\n", coins);
+      printf("%d \n",coins);
+
 }
