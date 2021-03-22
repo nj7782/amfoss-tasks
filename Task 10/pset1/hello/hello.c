92b2160295ab445ab2a9501c8fe5a6eda28e5e07
Jitin <73245324+nj7782@users.noreply.github.com>
2021-03-22 16:00:27 +0530

    Update hello.c

--- Task 10/pset1/hello/hello.c
+++ Task 10/pset1/hello/hello.c
@@ -1,11 +1,7 @@
-#include <stdio.h>
-#include <cs50.h>
-
-// Program asks for user's name and then greets him/her
-int main(void)
+#include<stdio.h>
+int main ()
 {
-    //get_string allows for user input that gets saved in the var name
-    string name = get_string("What is your name?\n");
-    //printf just outputs a message to the terminal
-    printf("hello, %s\n", name);
+    printf("hello,world\n");
+
+    return 0;
 }
